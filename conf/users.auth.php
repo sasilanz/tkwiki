288c45d9d13fffac8093712d93da948a81f8faec
Astrid Lanz <astrid.lanz@gmail.com>
2019-01-04 15:13:35 +0100

    first commit

---
+++ conf/users.auth.php
@@ -0,0 +1,13 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+admin:$1$cpQsxyRM$4KL7x4KloCa8ZPjTplsQZ0:astrid:admin@commuhnicat.com:admin,user
+tanja:$1$6BWMeRn1$eh14TbbRqbS8qHrKn4sPh.:Tanja:tanja@dolittle.ch:user
